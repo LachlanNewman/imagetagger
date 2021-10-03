3b3373886394e336c0eeec50d36769e1597d7c8e
Big L <lachlann@thearcsgroup.com>
2021-10-03 16:20:14 +0800

    feat: stub update route for single image

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,7 +1,15 @@
 import { Router , Request, Response, NextFunction } from "express";
 
 const router = Router()
-const images = [{
+
+export interface Image {
+    id: string;
+    title: string;
+    desc: String,
+    tags: string[];
+};
+
+const images: Image[] = [{
     id: "0",
     title: "testtitle",
     desc: "testDesc",
@@ -33,6 +41,17 @@ router.get("/:id",async (req:Request,res: Response,next:NextFunction) => {
     const id = parseInt(req.params.id);
     const image = images[id]
     res.send(image)
+})
+
+router.put("/:id",async (req:Request,res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    const title = req.body.title;
+    const desc = req.body.desc;
+    const image = images[id]
+    image.title = title;
+    image.desc = desc;
+    res.statusCode = 201;
+    res.send(image);
 })
 
 router.delete("/:id",async (req:Request,res: Response, next: NextFunction) => {
