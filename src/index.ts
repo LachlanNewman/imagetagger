ab184ebe7a3ddafef982075f7e22ed9040d02093
Big L <lachlann@thearcsgroup.com>
2021-10-03 15:47:26 +0800

    feat: stub route for creating a new image

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express from "express"
 import config from "./config";
 import routes from "./routes"
 
-const server = express()
 const port = config.PORT;
 
+const server = express()
+
+server.use(express.urlencoded({extended: true}));
+server.use(express.json())
 server.use(routes);
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,14 +1,30 @@
 import { Router , Request, Response, NextFunction } from "express";
 
 const router = Router()
+const images = [{
+    title: "testtitle",
+    desc: "testDesc",
+    tags: ["tag1", "tag2"]
+}]
 
-router.get("/", async (req:Request,res: Response,next:NextFunction) => {
-    const images = [{
-        title: "testtitle",
-        desc: "testDesc",
-        tags: ["tag1", "tag2"]
-    }]
+router.get("/", (req:Request,res: Response,next:NextFunction) => {
+
     res.send(images)
+})
+
+router.post("/",(req:Request,res: Response, next: NextFunction) => {
+    const title = req.body.title;
+    const desc = req.body.desc;
+    images.push({
+        title,
+        desc,
+        tags: []
+    })
+    res.statusCode = 201;
+    res.send({
+        title,
+        desc,
+    })
 })
 
 export default router;
